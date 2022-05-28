bd653098e10e38f814097aaef4677bc004718999
ssanay88 <pplloo748@gmail.com>
2022-05-28 23:36:59 +0900

    2022 - 05 - 28 MVVM 아키텍쳐 모델 연습 및 과제 학습

---
+++ app/src/main/java/com/example/k_moocclassinfoapp/acitivities/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.k_moocclassinfoapp.acitivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.k_moocclassinfoapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
