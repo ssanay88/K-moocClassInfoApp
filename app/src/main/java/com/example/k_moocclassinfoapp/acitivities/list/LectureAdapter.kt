774899838b530eaf69f0462d8e3b96f432f26020
ssanay88 <pplloo748@gmail.com>
2022-05-31 23:50:02 +0900

    2022 - 05 - 31 RecyclerView 공부

---
+++ app/src/main/java/com/example/k_moocclassinfoapp/acitivities/list/LectureAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.k_moocclassinfoapp.acitivities.list
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class LectureAdapter : ListAdapter<Lecture , LectureAdapter.LectureViewHolder> {
+
+
+
+    inner class LectureViewHolder : RecyclerView.ViewHolder {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LectureAdapter.LectureViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: LectureAdapter.LectureViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/k_moocclassinfoapp/acitivities/list/ListActivity.kt
@@ -0,0 +1,3 @@
+package com.example.k_moocclassinfoapp.acitivities.list
+
+class ListActivity {}
